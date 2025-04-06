9b07038432e0eb49811f874b38972378b6f21058
nninyeong <minyk0512@gmail.com>
2025-04-07 01:07:10 +0900

    chore: 기본 폰트 제거 및 pretendard 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,9 @@ import localFont from 'next/font/local';
 import './globals.css';
 import QueryProvider from '@/components/providers/QueryProvider';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
   weight: '100 900',
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${pretendard.variable} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
